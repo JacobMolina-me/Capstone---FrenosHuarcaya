e196798e5a0f6c6abdae337986b10846ad846a5d
JacobMolina-me <65180713+JacobMolina-me@users.noreply.github.com>
2020-10-13 16:43:52 -0500

    render productos

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from .models import *  
+
 def store(request):
-     context = {}
-     return render(request, 'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
+
 
 def cart(request):
      context = {}
